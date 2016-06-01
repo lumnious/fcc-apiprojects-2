b3e84a6cf8c7bfb8467f64844366ad3949db704f
lumnious <dotico@gmail.com>
2016-06-01 15:59:07 +0100

    Testing request available fields

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send(
+  	'<br>lang: ' + req.get('Accept-Language') +
+  	'<br>test: ' + req.get('Host') +
+  	'<br>test: ' + req.get('User-Agent') +
+  	'<br>test: ' + JSON.stringify(req.headers) +
+  	'<br>test: ' + req.ip +
+  	'<br>test: ' + req.ips
+  	)
 });
 
 module.exports = router;
